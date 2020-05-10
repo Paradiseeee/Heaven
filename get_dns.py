7075b5240112dac55e1caf5cf916ea60c284cd0b
paradiseeee <2649687318@qq.com>
2020-05-10 23:05:21 +0800

    Update get_dns.py

--- get_dns.py
+++ get_dns.py
@@ -1,7 +1,14 @@
 ﻿# -*- coding: utf-8 -*-
 
+
 class DNSParser():
 
     def __init__(self, domain):
         '''Initializing'''
         self.domain = domain
+
+
+if __name__ == '__main__':
+
+    parser = DNSParser('github.com')
+    print(parser.domain)
