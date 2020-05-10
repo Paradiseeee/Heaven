6783952f0a7fb16d7bce3ef7fbfe98ec285d2084
paradiseeee <2649687318@qq.com>
2020-05-11 06:28:48 +0800

    Update get_dns.py

--- get_dns.py
+++ get_dns.py
@@ -1,14 +1,29 @@
 ﻿# -*- coding: utf-8 -*-
+from pandas import read_html
 
 
 class DNSParser():
+    '''自动获取域名的dns并修改host文件'''
 
     def __init__(self, domain):
         '''Initializing'''
         self.domain = domain
+        self.url = f'https://{domain}.ipaddress.com'
+    
+    def parse_result(self):
+        dfs = read_html(self.url)
+        return dfs[0]
+
+    def write_host(self, filepath=''):
+        pass
 
 
 if __name__ == '__main__':
 
     parser = DNSParser('github.com')
-    print(parser.domain)
+    print('\n>>> github.com\n' + '-'*80)
+    print(parser.parse_result().to_markdown())
+
+    print('\n>>> github.global.ssl.fastly.net\n' + '-'*80)
+    parser.url = 'https://fastly.net.ipaddress.com/github.global.ssl.fastly.net'
+    print(parser.parse_result().to_markdown())
