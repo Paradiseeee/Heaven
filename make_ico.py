e2c5ca69e54b231373aa6f52b3d51d5cc415faba
paradiseeee <2649687318@qq.com>
2020-05-11 20:12:22 +0800

    Update make_ico.py

--- make_ico.py
+++ make_ico.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
+from PythonMagick import Image
+
 
 class ICOGenerator():
     '''图片转为 ICO'''
 
-    def __init__(self):
+    def __init__(self, filename, path='./'):
         '''Initializing'''
-        
+        self.path = './'
+        self.filename = filename
+    
+    def generate(self):
+        os.chdir(self.path)
+        img = Image(self.filename)
+        img.sample("128x128")
+        name = self.filename.split('.')[0]
+        img.write(name + '.ico')
+
+
+if __name__ == "__main__":
+
+    name = input("请输入图片名字（当前路径下）：\n")
+    generator = ICOGenerator(name)
