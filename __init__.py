500723b2c4086a23843ae5263bf417e324c12891
paradiseeee <2649687318@qq.com>
2020-05-11 21:07:52 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+DataScience Toolkits
+"""
 
-doc = """
+__doc__ = '''
 I am trying to build a module for specific field of data science.
 At the same time learn to manage a public project.
 
@@ -12,15 +15,14 @@ the next version.I am new to this whole thing and I look forward some
 usefull advices.
 
 If anyone is interested to this, please let me know:
-paradise-yang@outlook.com; paradise26472@qq.com;
+paradise26472@qq.com;
+paradise-yang@outlook.com; 
 
 More Information About This Module:
 https://github.com/paradiseeee/Heaven
 
 My GitHub Page:
-https://paradiseeee.github.io
-
-"""
+https://paradiseeee.github.io'''
 
 __author__ = "Paradise (github.com/paradiseeee)"
 __version__ = "1.0"
@@ -28,11 +30,33 @@ __copyright__ = "Copyright (c) 2019-2020 Paradise"
 # Use of this source code is governed by the MIT license.
 __license__ = "MIT"
 
+import os
+
 from .mysql_helper import MysqlHelper
 from .pdf_processor import PDFProcessor
 from .log_reader import LogReader
 from .get_dns import DNSParser
 
+
+def get_license():
+    with open('LICENSE', 'r') as f:
+        text = f.read()
+    print(text)
+
+def get_modules():
+    files = [i.split('.') for i in sorted(os.listdir())]
+    for f in files:
+        if 'py' in f:
+            print('\t' + '.'.join(f))
+    for f in files:
+        if len(f) == 1 and f[0] not in ['__pycache__', 'LICENSE']:
+            print(f[0] + '/')
+
+
 if __name__ == "__main__":
+    
     print(' Welcome to Heaven! '.center(72, '_'))
-    print(doc)
+    print(__doc__)
+
+    print(' Module List '.center(72, '_'))
+    get_modules
