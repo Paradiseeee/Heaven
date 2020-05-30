4317b6230841e02691a8fd99408adcb151da9b0c
paradiseeee <2649687318@qq.com>
2020-05-31 03:08:27 +0800

    Update make_ico.py

--- util/make_ico.py
+++ util/make_ico.py
@@ -22,6 +22,6 @@ class ICOGenerator():
 
 if __name__ == "__main__":
 
-    name = input("请输入图片名字（当前路径下）：\n")
-    generator = ICOGenerator(name)
+    path = input("请输入图片路径及文件名：\n")
+    generator = ICOGenerator(path)
     generator.generate()
