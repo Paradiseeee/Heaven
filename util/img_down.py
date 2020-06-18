475b1fbd3a72e4b5172b44d31076f4062128439b
paradiseeee <2649687318@qq.com>
2020-06-19 01:33:19 +0800

    Add util.img_down.py

---
+++ util/img_down.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Image DownLoader | Created to auto catch bing wallpaper"""
+import re
+import time
+import datetime
+import requests as rq
+from bs4 import BeautifulSoup
+
+
+class DownLoader():
+    '''[to-be-finished] download image from url'''
+    
+    def __init__(self):
+        '''def params for downloader'''
+        self.attrs = None
+    
+    def get(self, url, file):
+        '''download from image-url'''
+
+        print(f'\nDownloading {url}\n')
+        res = rq.get(url, stream=True)
+        with open(file, 'wb') as f:
+            f.write(res.content)
+
+
+def get_bing_bg():
+    '''get url of bing-home-page background'''
+
+    host = 'https://cn.bing.com'
+    res = rq.get(host)
+    soup = BeautifulSoup(res.text, features='lxml')
+    text = str(soup.find_all('div', {'id': 'bgDiv'})[0])
+    url = re.compile('src="(.*?)"', re.S).findall(text)[0].split('&')[0]
+
+    return host + url
+
+
+if __name__ == "__main__":
+
+    url = get_bing_bg()
+    date_str = str(datetime.date.today())
+    file = f"C:/Users/Paradise/Pictures/BING/{date_str}.{url.split('.')[-1]}"
+    DownLoader().get(url, file)
+    # useless sh*t
+    for i in range(75):
+        text = '=' * i
+        print('\r %s' % text, end='》')
+        time.sleep(0.01)
+    print(f'\n\nImage saved to {file}\n')
