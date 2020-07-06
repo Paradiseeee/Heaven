3fcc93e5902906f76e2cd79b5213120a36d8e542
paradiseeee <2649687318@qq.com>
2020-07-07 01:58:16 +0800

    Create translater.py

---
+++ util/translater.py
@@ -0,0 +1,28 @@
+﻿# -*- coding: utf-8 -*-
+import re
+import json
+import win32clipboard as wc
+from win32con import CF_TEXT
+
+# http://api.fanyi.baidu.com/doc/21
+
+def getclipboard():
+
+    wc.OpenClipboard()
+    text = wc.GetClipboardData(CF_TEXT)
+    wc.CloseClipboard()
+    try:
+        return text.decode()
+    except:
+        return text.decode('gbk')
+
+def remove_esc(text):
+
+    for t in ['\r', '\n', '\t']:
+        text = text.replace(t, ' ')
+    return text
+
+
+if __name__ == "__main__":
+
+    print(remove_esc(getclipboard()))
