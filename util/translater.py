c23d99e392345a7f50203e86a1ead55a6783c4a2
paradiseeee <2649687318@qq.com>
2020-07-11 21:19:59 +0800

    Update translater.py

--- util/translater.py
+++ util/translater.py
@@ -1,28 +1,40 @@
 ﻿# -*- coding: utf-8 -*-
+"""
+REFER | <http://api.fanyi.baidu.com/doc>
+"""
 import re
 import json
+import requests as rq
 import win32clipboard as wc
 from win32con import CF_TEXT
+from Heaven import __ROOT__
 
-# http://api.fanyi.baidu.com/doc/21
 
-def getclipboard():
+class Translater():
+    '''调用百度API英汉互译'''
 
-    wc.OpenClipboard()
-    text = wc.GetClipboardData(CF_TEXT)
-    wc.CloseClipboard()
-    try:
-        return text.decode()
-    except:
-        return text.decode('gbk')
+    def __init__(self, mode='cmd'):
+        '''initialization'''
 
-def remove_esc(text):
+        self.MODE = mode
+        with open(__ROOT__+'/_UserKeys/baidu-trans-key.json', 'r') as f:
+            self.KEYS = json.loads(f.read())
 
-    for t in ['\r', '\n', '\t']:
-        text = text.replace(t, ' ')
-    return text
+    def getclipboard(self):
+
+        wc.OpenClipboard()
+        text = wc.GetClipboardData(CF_TEXT)
+        wc.CloseClipboard()
+        try:
+            text = text.decode()
+        except:
+            text = text.decode('gbk')
+        for t in ['\r', '\n', '\t']:
+            text = text.replace(t, ' ')
+        
+        return text
 
 
 if __name__ == "__main__":
 
-    print(remove_esc(getclipboard()))
+    print(Translater().getclipboard())
