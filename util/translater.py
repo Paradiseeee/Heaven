d79a70f37e90fbd89f9d368ee5b75bf6129c9a98
paradiseeee <2649687318@qq.com>
2020-07-11 22:03:16 +0800

    Update translater.py

--- util/translater.py
+++ util/translater.py
@@ -4,6 +4,8 @@ REFER | <http://api.fanyi.baidu.com/doc>
 """
 import re
 import json
+import random
+import hashlib
 import requests as rq
 import win32clipboard as wc
 from win32con import CF_TEXT
@@ -18,7 +20,25 @@ class Translater():
 
         self.MODE = mode
         with open(__ROOT__+'/_UserKeys/baidu-trans-key.json', 'r') as f:
-            self.KEYS = json.loads(f.read())
+            self.API = json.loads(f.read())
+        self.URL = self.generate_url()
+
+    def generate_url(self):
+        '''生成请求链接'''
+
+        # 全为英文时使用英译中，否则中译英
+        query = self.getclipboard()
+        bool_map = map(lambda s: '\u4e00'<=s<='\u9fa5', query)
+        lang = ('en', 'zh') if sum(bool_map) == 0 else ('zh', 'en')
+        salt = str(random.randint(32768, 65536))
+        sign = self.API['API_ID'] + query + salt + self.API['API_KEY']
+        sign = hashlib.md5(sign.encode()).hexdigest()
+
+        url = 'http://api.fanyi.baidu.com/api/trans/vip/translate?'
+        url += f'q={query}&from={lang[0]}&to={lang[1]}'
+        url += f'&appid={self.API['API_ID']}&salt={salt}&sign={sign}'
+
+        return url
 
     def getclipboard(self):
 
