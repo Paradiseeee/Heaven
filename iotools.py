972980daa2160db753e43f08c51b439b51bc3d6f
paradiseeee <2649687318@qq.com>
2023-04-26 19:49:32 +0800

    Update iotools.py

--- iotools.py
+++ iotools.py
@@ -23,6 +23,22 @@ def iterfiles(root, depth, *suffix):
 
     return list(output.loc[index])
 
+def walkdir(top):
+    '''Return a list of all folders and a list of files'''
+
+    gen = os.walk(top)
+    files = []
+    folders = []
+    for root, folder, file in gen:
+        for f in file:
+            files.append(root + '/' + f)
+        for d in folder:
+            folders.append(root + '/' + d)
+    files = [f.replace('\\', '/').replace('//', '/') for f in files]
+    folders = [d.replace('\\', '/').replace('//', '/') for d in folders]
+    
+    return files, folders
+
 
 if __name__ == "__main__":
 
